c3f755c27f6d63930f754412035461fcd93c4ca1
Haens90 <haens90@hotmail.de>
2023-06-03 22:52:54 +0200

    First Board

--- game.py
+++ game.py
@@ -3,15 +3,30 @@ import pygame
 pygame.init()
 
 #defining size of game window
-windowsSize = pygame.display.set_mode((800,600)) 
+window = pygame.display.set_mode((800,600)) 
 pygame.display.set_caption("Hello World Printer")
 
 #defining font attributes
-myFont = pygame.font.SysFont("Segoe UI", 90)
-helloWorld = myFont.render("Hello World", 1, (255, 0, 255), (255, 255, 255))
+font_size = 90
+headline_font = pygame.font.SysFont("Segoe UI", font_size)
+headline = headline_font.render("ChessVenture", 1, (255, 0, 255), (255, 255, 255))
 
 while 1:
     for event in pygame.event.get():
         if event.type==pygame.QUIT: sys.exit()
-    windowsSize.blit(helloWorld, (0, 0))
+
+    #draw fields
+    board_height = window.get_height() - font_size
+    field_size = min(board_height, window.get_width()) / 8
+    for i in range(8):
+        for j in range(int(window.get_width() / field_size)):
+            row = field_size * i
+            col = field_size * j
+            if((i + j) % 2 == 0):
+                color = 'white'
+            else:
+                color = 'black'
+            pygame.draw.rect(window, color, pygame.Rect(col, row + font_size, field_size, field_size))
+
+    window.blit(headline, (0, 0))
     pygame.display.update()
