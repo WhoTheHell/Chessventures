1c4da1d377fa512c4ea77febc97730be7e02afb1
Haens90 <haens90@hotmail.de>
2023-06-08 13:20:55 +0200

    added movement functionality and sprite image

--- game.py
+++ game.py
@@ -6,14 +6,33 @@ import tmpfigure
 pygame.init()
 
 #defining size of game window
-window = pygame.display.set_mode((800,600)) 
+clock = pygame.time.Clock()
+window_surface = pygame.display.set_mode((800,600)) 
 pygame.display.set_caption("ChessVenture")
 
 #defining font attributes
 font_size = 90
+infobox_height = 12
 headline_font = pygame.font.SysFont("Segoe UI", font_size)
+infobox_font = pygame.font.SysFont("Segoe UI", infobox_height)
 headline = headline_font.render("ChessVenture", 1, (255, 0, 255), (255, 255, 255))
 font_size = headline.get_height()
+infobox = infobox_font.render("Infobox", 1, 'white')
+    
+#game assets
+board_height = window_surface.get_height() - font_size
+field_size = min(board_height, window_surface.get_width()) / 8
+
+actor_width = 20
+actor = tmpfigure.TmpFigure(actor_width, actor_width)
+actor.rect.x = random.randrange(0,4) * field_size + field_size * 0.5 - actor_width / 2
+actor.rect.y = random.randrange(0,8) * field_size + field_size * 0.5 + font_size - actor_width / 2
+# actor.set_
+
+all_sprites_list = pygame.sprite.Group()
+all_sprites_list.add(actor)
+
+frame_counter = 0
 
 while 1:
     for event in pygame.event.get():
@@ -21,31 +40,39 @@ while 1:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE: sys.exit()
 
+    headline = headline_font.render("ChessVenture ", 1, (255, 0, 255), (255, 255, 255))
+    infobox_text = [
+        "current pixel " + str(actor.rect.x) + ", " + str(actor.rect.y),
+        "target pixel " + str(actor.target_location.x) + ", " + str(actor.target_location.y),
+        "target field " + str(actor.col) + ", " + str(actor.row),
+        "target field " + str(actor.target_location.x) + ", " + str(actor.target_location.y)
+    ]
+    labels = []
+    for line in infobox_text:
+        labels.append(infobox_font.render(line, 1, 'white', 'black'))
+        
+    for line_nr in range(len(labels)):
+        window_surface.blit(labels[line_nr], (headline.get_width(), line_nr * (infobox_height + 15)))
+    # for label in labels:
+    #     window_surface.blit(label, (headline.get_width(), infobox_height))
+    window_surface.blit(headline, (0, 0))
+
     #draw fields
-    board_height = window.get_height() - font_size
-    field_size = min(board_height, window.get_width()) / 8
     for i in range(8):
-        for j in range(int(window.get_width() / field_size)):
+        for j in range(int(window_surface.get_width() / field_size)):
             row = field_size * i
             col = field_size * j
             if((i + j) % 2 == 0):
                 color = 'white'
             else:
                 color = 'black'
-            pygame.draw.rect(window, color, pygame.Rect(col, row + font_size, field_size, field_size))
+            pygame.draw.rect(window_surface, color, pygame.Rect(col, row + font_size, field_size, field_size))
 
-    actor_width = 20
-    actor = figure.TmpFigure('red', actor_width, actor_width)
-    actor.rect.x = random.randrange(0,4) * field_size + field_size * 0.5 - actor_width / 2
-    actor.rect.y = random.randrange(0,8) * field_size + field_size * 0.5 + font_size - actor_width / 2
-
-    all_sprites_list = pygame.sprite.Group(actor)
-   # all_sprites_list.add(figure)
-
-    all_sprites_list.draw(window)
-
-    window.blit(headline, (0, 0))
+    if(frame_counter % 300 == 0):
+        actor.move(random.randrange(0,4),random.randrange(0,8), field_size, font_size)
+    all_sprites_list.update()
+    all_sprites_list.draw(window_surface)
     pygame.display.update()
 
-    clock = pygame.time.Clock()
+    frame_counter += 1
     clock.tick(60)
--- tmpfigure.py
+++ tmpfigure.py
@@ -1,11 +1,44 @@
 import pygame
+from pygame.rect import Rect
+import os
+import math
 
 class TmpFigure(pygame.sprite.Sprite):
 
-   def __init__(self, color, width, height):
+   def __init__(self, width, height):
       super().__init__()
 
-      self.image = pygame.Surface([width, height])
-      self.image.fill(color)
+      self.image = pygame.image.load(os.path.join('res', 'strawberry.jpg'))
+      self.image = pygame.transform.scale(self.image, (width, height))
+      # self.image = pygame.Surface([width, height])
+      self.move_speed = 1 #pixel per frame
 
       self.rect = self.image.get_rect()
+      self.target_location = Rect(self.rect)
+      self.target_location.x = self.rect.x
+      self.target_location.y = self.rect.y
+      self.row = 0
+      self.col = 0
+
+   def update(self):
+      self.update_pos()
+
+   def update_pos(self):
+      x_dist = self.rect.x - self.target_location.x
+      y_dist = self.rect.y - self.target_location.y
+      # slope = x_dist / y_dist
+      euklidean_dist = math.sqrt(x_dist * x_dist + y_dist * y_dist)
+      if(euklidean_dist != 0):
+         ratio = euklidean_dist / self.move_speed
+         new_x_increment = x_dist / ratio
+         new_y_increment = y_dist / ratio
+
+         self.rect.x -= new_x_increment
+         self.rect.y -= new_y_increment
+
+   def move(self, _col, _row, offset, header_row):
+      #todo missing field_size
+      self.col = _col
+      self.row = _row
+      self.target_location.x = self.col * offset + offset * 0.5 - self.image.get_width() / 2
+      self.target_location.y = self.row * offset + offset * 0.5 + header_row - self.image.get_width() / 2
